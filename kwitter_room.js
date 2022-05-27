cc3de4a09d2f04e445cd52e858bb597e71a23769
Aarush2011 <104634345+Aarush2011@users.noreply.github.com>
2022-05-27 11:04:18 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyArslEcJ1R0HqrYF33BdG3TSCFEYzLwGeY",
+  authDomain: "letschat-44b9d.firebaseapp.com",
+  databaseURL: "https://letschat-44b9d-default-rtdb.firebaseio.com",
+  projectId: "letschat-44b9d",
+  storageBucket: "letschat-44b9d.appspot.com",
+  messagingSenderId: "1003746733595",
+  appId: "1:1003746733595:web:1e94b0656aa59d898a52be",
+  measurementId: "G-GWBW6CKRM8"
+};
+firebase.initializeApp(firebaseConfig);
+
+
